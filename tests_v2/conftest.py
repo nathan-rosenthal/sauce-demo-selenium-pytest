bd06774b5919f4358552f7002f82bc77021d5728
nathan-rosenthal <nathan.nandor@gmail.com>
2024-02-13 22:06:11 +0200

    Disable teardown function take 2

--- tests_v2/conftest.py
+++ tests_v2/conftest.py
@@ -19,10 +19,10 @@ STANDARD_USERNAME = 'standard_user'
 PASSWORD_ALL = 'secret_sauce'
 
 
-def pytest_exception_interact(report):
-    if report.failed:
-        allure.attach(body=driver.get_screenshot_as_png(), name="screenshot",
-                      attachment_type=allure.attachment_type.PNG)
+# def pytest_exception_interact(report):
+#     if report.failed:
+#         allure.attach(body=driver.get_screenshot_as_png(), name="screenshot",
+#                       attachment_type=allure.attachment_type.PNG)
 
 
 # def pytest_sessionfinish() -> None:
